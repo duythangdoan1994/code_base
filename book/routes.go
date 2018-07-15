f297ef83e4cdfced94660bfd376dbd6b476c27a6
thangdd <duythangdoan1994@gmail.com>
2018-07-15 16:36:38 +0700

    code base

--- book/routes.go
+++ book/routes.go
@@ -1,7 +1,10 @@
 package book
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/luciandd/core/book/views"
+)
 
 func RegisterBook(router *gin.RouterGroup) {
-	router.GET("/", ListBook)
+	router.GET("/", bookviews.ListBook)
 }
--- book/serializers.go
+++ book/serializers.go
@@ -1,16 +1,44 @@
 package book
 
 import (
-	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 )
 
-func ListBook(c *gin.Context) {
-	//var books []Book
-	//err := GetAllBook(db, &books)
-	//if err != nil {
-	//	respondError(w, http.StatusInternalServerError, err.Error())
-	//	return
-	//}
-	//respondJSON(w, http.StatusOK, books)
-	c.JSON(200, gin.H{"ok": "Welcome to Chicago!"})
+func (b *Book) TableName() string {
+	return "books"
+}
+
+func InsertBook(db *gorm.DB, b *Book) (err error) {
+	if err = db.Save(b).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetAllBook(db *gorm.DB, b *[]Book) (err error) {
+	if err = db.Order("id desc").Find(b).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func OneBookGetting(db *gorm.DB, ids int, b *Book) (err error) {
+	if err := db.Where("codes = ?", ids).First(&b).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func UpdateBook(db *gorm.DB, b *Book) (err error) {
+	if err = db.Save(b).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeletedBook(db *gorm.DB, b *Book) (err error) {
+	if err = db.Delete(b).Error; err != nil {
+		return err
+	}
+	return nil
 }
